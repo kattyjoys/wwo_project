dc455d922c5e270f451625e8159814844b163024
Katya <katya.nasevich@gmail.com>
2015-10-31 01:19:48 +0300

    page to_15
    finished

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,23 @@
-//$( ".collapse-btn" ).click(function() {
-//  $( ".no-visible" ).slideToggle( "slow" );
+//$( ".mobile-to_15 .collapse-btn" ).click(function() {
+//  $( ".mobile-to_15 tbody" ).slideToggle( "slow" );
+//  $( ".mobile-to_15 .collapse-btn" ).removeClass( "collapsed" )
+//});
+
+
+
+//$tableHeight = $(".table.mobile-to_15" ).height();
+//h = $tableHeight.height();
+//$(".mobile-to_15 .collapse-btn").css('height',h-20);
+
+//var tableHeight = $(".table.mobile-to_15" ).height();
+//$(".mobile-to_15 .collapse-btn").height( tableHeight );
+
+
+
+
+
+
+//$(".mobile-to_15 .collapse-btn").on( "click", function() {
+//  var tableHeight = $(".table.mobile-to_15" ).height();
+//  $(".mobile-to_15 .collapse-btn").height( tableHeight );
 //});
